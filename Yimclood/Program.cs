e6522b3359b2ca0dfe281c7df03b1f90ff77643c
Murif777 <faridelcapo0107@gmail.com>
2024-06-03 09:04:36 -0500

    Confirmacion

--- Yimclood/Program.cs
+++ Yimclood/Program.cs
@@ -14,7 +14,7 @@ namespace Yimclood
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new Login());
+            Application.Run(new Registrar());
         }
     }
 }
